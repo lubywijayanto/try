4138153e1c754e0edcf38167e47bd20ce8a05168
lubywijayanto <lubywijayanto@gmail.com>
2018-07-30 09:56:16 +0700

    Update roll.js

--- roll.js
+++ roll.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="5-kesalahan-saat-menggosok-gigi-yang.html"}
 if(x=="2"){xxx="5-kesalahan-kesehatan-yang-sering.html"}
 if(x=="3"){xxx="avp_57.html"}
 if(x=="4"){xxx="avp_29.html"}
-if(x=="5"){xxx="avp.html"}a_to_ve[i].href="http://ayo-download-safelink.blogspot.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="avp.html"}a_to_ve[i].href="https://ayo-download-safelink.blogspot.com/2018/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
